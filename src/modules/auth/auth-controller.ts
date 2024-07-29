3d89014058c39a20c7b733b800112190383ae1d0
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-29 20:48:51 -0300

    feat(auth): add auth-controller

---
+++ src/modules/auth/auth-controller.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from "express";
+import { LoginService } from "./services/login-service";
+import { JWTHandler } from "../../lib/jwt-handler";
+import { SignupService } from "./services/signup-service";
+
+export class AuthController {
+  login = async (req: Request, res: Response) => {
+    const data = req.body;
+    const userID = await new LoginService().execute(data);
+    const { accessToken, refreshToken } = await this.generateTokens(userID);
+    res.setHeader("authorization", `Bearer ${accessToken}`);
+    res.cookie("refresh-token", refreshToken, {
+      httpOnly: true,
+      sameSite: "lax",
+      path: "/",
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    });
+    res.json({ message: "User successfully signed in." });
+  };
+
+  signup = async (req: Request, res: Response) => {
+    const data = req.body;
+    const userID = await new SignupService().execute(data);
+    const { accessToken, refreshToken } = await this.generateTokens(userID);
+    res.setHeader("authorization", `Bearer ${accessToken}`);
+    res.cookie("refresh-token", refreshToken, {
+      httpOnly: true,
+      sameSite: "lax",
+      path: "/",
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    });
+    res.json({ message: "User successfully signed up." });
+  };
+
+  private async generateTokens(userID: string) {
+    const accessToken = await JWTHandler.generateAccessToken(userID);
+    const refreshToken = await JWTHandler.generateRefreshToken(userID);
+    return { accessToken, refreshToken };
+  }
+}

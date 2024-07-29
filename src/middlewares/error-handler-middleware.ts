9f4bda4eb8255bc79b9113b49a62269100d1f78d
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-29 20:45:59 -0300

    feat(middlewares): add error-handler-middleware

---
+++ src/middlewares/error-handler-middleware.ts
@@ -0,0 +1,48 @@
+import { NextFunction, Request, Response } from "express";
+import { ZodError } from "zod";
+import { AuthenticationError } from "../errors/authentication-error";
+import { AuthorizationError } from "../errors/authorization-error";
+import { ClientError } from "../errors/client-error";
+import { env } from "../lib/env";
+
+export function errorHandlerMiddleware(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (env.NODE_ENV == "development") console.log(err);
+
+  if (err instanceof ZodError) {
+    return res.status(400).json({
+      message: "Invalid input.",
+      errors: err.flatten().fieldErrors
+    });
+  }
+
+  if (err instanceof ClientError) {
+    return res.status(400).json({
+      message: err.message ?? "Invalid input."
+    });
+  }
+
+  if (err instanceof AuthenticationError) {
+    return res.status(401).json({
+      message: err.message ?? "Unauthenticated access denied."
+    });
+  }
+
+  if (err instanceof AuthorizationError) {
+    return res.status(403).json({
+      message: err.message ?? "Unauthorized access denied."
+    });
+  }
+
+  if (err.message.includes("Unique constraint failed on the fields: (`email`)")) {
+    return res.status(400).json({
+      message: "Email already in use."
+    });
+  }
+
+  return res.status(500).json("Internal server error.");
+}

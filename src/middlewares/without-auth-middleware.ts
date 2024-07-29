81cdbf586be181fefc278f27b26032a7919c4c8b
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-29 20:46:22 -0300

    feat(middlewares): add without-auth-middleware

---
+++ src/middlewares/without-auth-middleware.ts
@@ -0,0 +1,8 @@
+import { Request, Response, NextFunction } from "express";
+import { AuthenticationError } from "../errors/authentication-error";
+
+export function withoutAuthMiddleware(req: Request, res: Response, next: NextFunction) {
+  const authorizationHeader = req.headers.authorization;
+  if (authorizationHeader) throw new AuthenticationError("User already signed in.");
+  next();
+}

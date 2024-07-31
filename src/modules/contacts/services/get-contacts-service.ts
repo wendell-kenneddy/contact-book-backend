025987c6f5fbe1adb3b6257b93eec9815683e889
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-31 20:56:24 -0300

    feat(get-contact-service): use cache when retrieving contact pages

--- src/modules/contacts/services/get-contacts-service.ts
+++ src/modules/contacts/services/get-contacts-service.ts
@@ -1,26 +1,30 @@
 import { z } from "zod";
 import { prisma } from "../../../lib/prisma";
+import { redis } from "../../../lib/redis";
 
 const queryPageDataSchema = z.object({
   userID: z.string().uuid("Invalid user ID."),
-  offset: z
-    .number()
-    .min(0, "Page offset can't be less 0.")
-    .int("Page offset must be an integer."),
-  size: z
-    .number()
-    .min(1, "Page size can't be less than 1.")
-    .max(10, "Page size can't be greater than 10.")
+  pageIndex: z.number().min(1, "Page index can't be less than 1.")
 });
 
 export class GetContactsService {
   async execute(data: unknown) {
-    const { userID, offset, size } = queryPageDataSchema.parse(data);
+    const { userID, pageIndex } = queryPageDataSchema.parse(data);
+    const cachedPage = await redis.get(`contacts:${userID}:${pageIndex}`);
+
+    if (cachedPage) return JSON.parse(cachedPage);
+
     const page = await prisma.contact.findMany({
       where: { owner_id: userID },
-      skip: offset,
-      take: size
+      skip: (pageIndex - 1) * 10,
+      take: 10,
+      orderBy: {
+        created_at: "desc"
+      }
     });
+    if (page.length) {
+      await redis.set(`contacts:${userID}:${pageIndex}`, JSON.stringify(page), "EX", 60 * 60 * 24);
+    }
 
     return page;
   }

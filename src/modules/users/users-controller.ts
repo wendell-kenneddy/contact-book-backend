c946f0c494496ba9c8ddf78b0103e08b49c738b8
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-08-22 19:55:32 -0300

    feat(users-controller): handle profile route

--- src/modules/users/users-controller.ts
+++ src/modules/users/users-controller.ts
@@ -1,7 +1,14 @@
 import { Request, Response } from "express";
 import { DeleteUserService } from "./services/delete-user-service";
+import { GetUserProfileService } from "./services/get-user-profile-service";
 
 export class UsersController {
+  getProfile = async (req: Request, res: Response) => {
+    const userID = (req as any).userID;
+    const profile = await new GetUserProfileService().execute(userID);
+    return res.json({ data: profile });
+  };
+
   delete = async (req: Request, res: Response) => {
     const userID = (req as any).userID;
     await new DeleteUserService().execute(userID);
